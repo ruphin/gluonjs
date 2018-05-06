f31fd3d9cf9f886f9e0887a3b0f2d6cbaa329dd6
Goffert van Gool <ruphin@ruphin.net>
2018-05-06 16:48:04 +0200

    Update build script, add comments, add license to files

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import filesize from 'rollup-plugin-filesize';
 import uglify from 'rollup-plugin-uglify';
-import { uglifier } from 'uglify-es';
 import babel from 'rollup-plugin-babel';
 import includePaths from 'rollup-plugin-includepaths';
 import resolve from 'rollup-plugin-node-resolve';
@@ -15,23 +14,28 @@ const includePathOptions = {
 function getConfig({ dest, format, uglified = true, transpiled = false, bundled = true }) {
   const conf = {
     input: 'src/gluon.js',
-    output: { exports: 'named', file: dest, format, name: 'GluonJS', sourcemap: true },
+    output: { exports: 'named', file: dest, format, name: 'GluonJS', sourcemap: uglified },
     external: bundled ? [] : [path.resolve('./lit-html/lib/lit-extended.js'), path.resolve('./lit-html/lib/shady-render.js')],
     plugins: [
       bundled && includePaths(includePathOptions),
       transpiled && resolve(),
       transpiled &&
-      commonjs({
-        include: 'node_modules/**'
-      }),
+        commonjs({
+          include: 'node_modules/**'
+        }),
       transpiled &&
-      babel({
-        presets: [['env', { modules: false }]],
-        plugins: ['transform-runtime'],
-        runtimeHelpers: true,
-        exclude: 'node_modules/**'
+        babel({
+          presets: [['env', { modules: false }]],
+          plugins: ['external-helpers']
+        }),
+      uglify({
+        warnings: true,
+        toplevel: uglified,
+        sourceMap: uglified,
+        compress: uglified && { passes: 2 },
+        mangle: uglified,
+        output: { beautify: !uglified }
       }),
-      uglified && uglify({ warnings: true, toplevel: !transpiled, sourceMap: true, compress: { passes: 2 }, mangle: { properties: false } }, uglifier),
       filesize()
     ].filter(Boolean)
   };
@@ -49,8 +53,7 @@ const example = ({ uglified = false } = {}) => {
         presets: [['env', { modules: false }]],
         plugins: ['external-helpers']
       }),
-      uglified &&
-      uglify({ warnings: true, keep_fnames: true, sourceMap: true, compress: { passes: 2 }, mangle: { properties: false, keep_fnames: true } }, uglifier),
+      uglified && uglify({ warnings: true, toplevel: true, keep_fnames: true, sourceMap: true, compress: { passes: 2 }, mangle: { keep_fnames: true } }),
       filesize()
     ].filter(Boolean)
   };
@@ -59,8 +62,7 @@ const example = ({ uglified = false } = {}) => {
 const config = [
   getConfig({ dest: 'gluon.es5.js', format: 'iife', transpiled: true }),
   getConfig({ dest: 'gluon.umd.js', format: 'umd' }),
-  getConfig({ dest: 'gluon.js', format: 'es', bundled: false }),
-  example({ uglified: false })
+  example({ uglified: true })
 ];
 
 export default config;
--- src/gluon.js
+++ src/gluon.js
@@ -1,22 +1,80 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2017 Goffert van Gool
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import { render } from '../lit-html/lib/shady-render.js';
 export { html } from '../lit-html/lib/lit-extended.js';
 
+// Key to store the HTML tag in a custom element class
 const TAG = Symbol('tag');
+
+// Key to store render status in a custom element instance
 const NEEDSRENDER = Symbol('needsRender');
 
+// Transforms a camelCased string into a kebab-cased string
 const camelToKebab = camel => camel.replace(/([a-z](?=[A-Z]))|([A-Z](?=[A-Z][a-z]))/g, '$1$2-').toLowerCase();
 
+// Creates an ID cache in the `$` property of a custom element instance
 const createIdCache = element => {
   element.$ = {};
-  element.shadowRoot.querySelectorAll('[id]').forEach(el => {
-    element.$[el.id] = el;
+  element.shadowRoot.querySelectorAll('[id]').forEach(node => {
+    element.$[node.id] = node;
   });
 };
 
+/**
+ * A lightweight base class for custom elements
+ *
+ * Features:
+ *
+ *  - Determines an appropriate HTML tagname based on an element's class name
+ *  - Efficient rendering engine using lit-html (https://github.com/Polymer/lit-html)
+ *  - Creates a cache for descendant nodes with an `id` in the `$` property
+ */
 export class GluonElement extends HTMLElement {
+  /**
+   * Returns the HTML tagname for elements of this class
+   *
+   * It defaults to the kebab-cased version of the class name. To override,
+   * defined a `static get is()` property on your custom element class, and return
+   * whatever string you want to use for the HTML tagname
+   */
   static get is() {
     return (this.hasOwnProperty(TAG) && this[TAG]) || (this[TAG] = camelToKebab(this.name));
   }
+
+  /**
+   * Called when an element is connected to the DOM
+   *
+   * When an element has a `template`, attach a shadowRoot to the element,
+   * and render the template. Once the template is rendered, creates an ID cache
+   * in the `$` property
+   *
+   * When adding a `connectedCallback` to your custom element, you should call
+   * `super.connectedCallback()` before doing anything other than actions
+   * that alter the result of the template rendering.
+   */
   connectedCallback() {
     if ('template' in this) {
       this.attachShadow({ mode: 'open' });
@@ -24,11 +82,15 @@ export class GluonElement extends HTMLElement {
       createIdCache(this);
     }
   }
-  // Render the element's template.
-  // By default, renders are asynchronous and executed at the next microtask timing,
-  // so multiple render calls are batched.
-  // If the sync option is set to `true`, it renders synchronously instead.
-  // Returns a Promise that resolves when the render is completed.
+
+  /**
+   * Renders the template for this element into the shadowRoot
+   *
+   * @param { sync }: perform a synchronous (blocking) render. The default render
+   *     is asynchronous, and multiple calls to `render()` are batched by default
+   *
+   * @returns a Promise that resolves once template has been rendered
+   */
   async render({ sync = false } = {}) {
     this[NEEDSRENDER] = true;
     if (!sync) {
